3d2580729efddedb0d9cf8f812f65e48ca881637
Zell Liew <zellwk@gmail.com>
2015-03-17 07:57:50 +0800

    add sourcemaps
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,13 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./css'));
 })
 
-gulp.task('watch', function() {
+// Create Gulp Default Task
+// ------------------------
+// Having watch within the task ensures that 'sass' has already ran before watching
+// 
+// This setup is slightly different from the one on the blog post at
+// http://www.zell-weekeat.com/gulp-libsass-with-susy/#comment-1910185635
+gulp.task('default', ['sass'], function () {
   gulp.watch('./scss/{,*/}*.{scss,sass}', ['sass'])
-})
+});
 
-gulp.task('default', ['sass', 'watch']);
